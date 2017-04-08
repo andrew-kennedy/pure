a89cf99a0153cc9469080113ffb56cfc8a7b6b5c
Andrew Kennedy <andrewk36@gmail.com>
2017-04-07 18:21:59 -0700

    color tweaks

--- fish_default_mode_prompt.fish
+++ fish_default_mode_prompt.fish
@@ -4,16 +4,16 @@ function fish_default_mode_prompt --description 'Display the default mode for th
         or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
         switch $fish_bind_mode
             case default
-                set_color --bold --background red white
+                set_color --bold brred
                 echo '❯'
             case insert
-                set_color --bold --background green white
+                set_color --bold brgreen
                 echo '❯'
             case replace-one
-                set_color --bold --background green white
+                set_color --bold brblue
                 echo '❯'
             case visual
-                set_color --bold --background magenta white
+                set_color --bold brmagenta
                 echo '❯'
         end
         set_color normal
